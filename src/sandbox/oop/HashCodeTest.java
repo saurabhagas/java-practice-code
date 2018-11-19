afe8309b80f7854517b10125d80b914092a90305
Saurabh Agarwal <Saurabh.Agarwal@softwareag.com>
2018-11-19 14:53:27 +0530

    Added new samples

--- src/sandbox/oop/HashCodeTest.java
+++
@@ -1,9 +0,0 @@
-package sandbox.oop;
-
-public class HashCodeTest {
-  public static void main(String[] args) {
-    HashCodeTest obj = new HashCodeTest();
-    System.out.println(obj);
-    System.out.println(obj.hashCode());
-  }
-}
