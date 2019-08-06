92f745bf7ce933dbced81e7ba0b0330d479c2093
Saurabh Agarwal <Saurabh.Agarwal@softwareag.com>
2019-08-06 11:32:29 +0530

    Count sorting algorithm

---
+++ algorithms/src/test/java/com/saurabh/algorithms/sorting/CountSorterTest.java
@@ -0,0 +1,16 @@
+package com.saurabh.algorithms.sorting;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CountSorterTest {
+  @Test
+  public void basicTest() {
+    CountSorter ob = new CountSorter();
+    char[] arr = {'G', 'E', 'E', 'K', 'S', 'F', 'O', 'R', 'G', 'E', 'E', 'K', 'S'};
+
+    char[] sorted = ob.sort(arr);
+    assertThat(sorted).containsExactly('E', 'E', 'E', 'E', 'F', 'G', 'G', 'K', 'K', 'O', 'R', 'S', 'S');
+  }
+}
