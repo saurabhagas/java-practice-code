f9d47c775be654be8512bbc65f07549c1c273020
Saurabh Agarwal <Saurabh.Agarwal@softwareag.com>
2019-08-06 10:39:45 +0530

    Better encapsulation for MixValueInStackInConstantTime class

--- basics/src/main/java/com/saurabh/basics/MinValueInStackInConstantTime.java
+++ basics/src/main/java/com/saurabh/basics/MinValueInStackInConstantTime.java
@@ -24,44 +24,44 @@ public class MinValueInStackInConstantTime {
     stack.pop();
     System.out.println(stack.min());
   }
-}
 
-class NewStack {
+  private static class NewStack {
 
-  //We will use two stacks mainStack to hold original values and minStack to hold minimum values.
-  //Top of minStack will always be the minimum value from mainStack
-  private Deque<Integer> mainStack;
-  private Deque<Integer> minStack;
+    //We will use two stacks mainStack to hold original values and minStack to hold minimum values.
+    //Top of minStack will always be the minimum value from mainStack
+    private final Deque<Integer> mainStack;
+    private final Deque<Integer> minStack;
 
-  public NewStack(int size) {
-    mainStack = new ArrayDeque<>(size);
-    minStack = new ArrayDeque<>(size);
-  }
-
-  //Removes and return value from newStack
-  //1. Pop element from minStack to make it sync with mainStack,
-  //2. Pop element from mainStack and return that value.
-  public int pop() {
-    minStack.pop();
-    return mainStack.pop();
-  }
-
-  //Pushes values into newStack
-  //1. Push value in mainStack and check value with the top value of minStack
-  //2. If value is greater than top, then push top in minStack
-  //else push value in minStack.
-  public void push(int value) {
-    mainStack.push(value);
-
-    if (!minStack.isEmpty() && value > minStack.peek()) {
-      minStack.push(minStack.peek());
-    } else {
-      minStack.push(value);
+    NewStack(int size) {
+      mainStack = new ArrayDeque<>(size);
+      minStack = new ArrayDeque<>(size);
     }
-  }
 
-  //Returns minimum value from newStack in O(1) Time
-  public int min() {
-    return minStack.peek();
+    //Removes and return value from newStack
+    //1. Pop element from minStack to make it sync with mainStack,
+    //2. Pop element from mainStack and return that value.
+    int pop() {
+      minStack.pop();
+      return mainStack.pop();
+    }
+
+    //Pushes values into newStack
+    //1. Push value in mainStack and check value with the top value of minStack
+    //2. If value is greater than top, then push top in minStack
+    //else push value in minStack.
+    void push(int value) {
+      mainStack.push(value);
+
+      if (!minStack.isEmpty() && value > minStack.peek()) {
+        minStack.push(minStack.peek());
+      } else {
+        minStack.push(value);
+      }
+    }
+
+    //Returns minimum value from newStack in O(1) Time
+    int min() {
+      return minStack.peek();
+    }
   }
 }
