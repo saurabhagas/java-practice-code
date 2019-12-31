68149e286bc9eda5a9f8f525a9b9d85b27be34d5
Swapna Kulkarni <skulkarni99@gatech.edu>
2020-01-01 00:15:19 +0530

    Added solution for Reverse of Words in a string

---
+++ geeksforgeeks-question-set/src/main/java/com/saurabh/interview/strings/_1_ReverseWordsInAString.java
@@ -0,0 +1,26 @@
+package com.saurabh.interview.strings;
+
+/**
+ * Problem - Given a String of length S, reverse the whole string without reversing the individual words in it. Words are separated by dots.
+ * Approach - O(n) time where n is numbers of words in the string.
+ *  Split the string into substrings with separator as "." and run a reverse loop on the array of split strings
+ */
+public class _1_ReverseWordsInAString {
+  public String reverseWords(String originalString) {
+    String[] strings = originalString.split("\\.");
+    StringBuffer reversedString = new StringBuffer();
+    for (int i = strings.length - 1; i >= 0; i--) {
+      if(!strings[i].isEmpty()) {
+        reversedString.append(strings[i] + ".");
+      }
+    }
+    reversedString.deleteCharAt(reversedString.length() - 1);
+    if (originalString.startsWith(".")) {
+      reversedString.insert(0, ".");
+    }
+    if (originalString.endsWith(".")) {
+      reversedString.append(".");
+    }
+    return reversedString.toString();
+  }
+}
