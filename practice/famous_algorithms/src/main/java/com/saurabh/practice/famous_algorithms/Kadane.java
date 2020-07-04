404d5f8d074131d8819b81e58cfc7f6019ed0d1d
Saurabh Agarwal <Saurabh.Agarwal@softwareag.com>
2020-07-04 15:48:02 +0530

    Simplification of Kadane implementation

--- practice/famous_algorithms/src/main/java/com/saurabh/practice/famous_algorithms/Kadane.java
+++ practice/famous_algorithms/src/main/java/com/saurabh/practice/famous_algorithms/Kadane.java
@@ -2,7 +2,7 @@ package com.saurabh.practice.famous_algorithms;
 
 /*
 Problem - Largest sum contiguous subarray. Problem at: https://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
-  Approach (O(n) time, O(1) space) using Kadane's algorithm:
+  Approach (O(n) time, O(1) space) using Kadane's algorithm (DP without extra space):
    1. currSum stores the sum of current numbers
    2. maxSum keeps track of max sum so far
    3. If currSum becomes negative, reset it
@@ -13,16 +13,17 @@ public class Kadane {
     int currSum = 0;
     int maxSum = Integer.MIN_VALUE;
 
+    // Standard DP solution using O(n) space
+//    for (int i = 1; i < nums.length; i++) {
+//      int num = nums[i];
+//      dp[i] = Math.max(dp[i - 1], 0) + num;
+//      maxSum = Math.max(currSum, maxSum);
+//    }
+
+    // O(n) space isn't really needed
     for (int num : nums) {
-      currSum += num;
-
-      if (currSum > maxSum) {
-        maxSum = currSum;
-      }
-
-      if (currSum < 0) {
-        currSum = 0;
-      }
+      currSum = Math.max(currSum, 0) + num;
+      maxSum = Math.max(currSum, maxSum);
     }
     return maxSum;
   }
