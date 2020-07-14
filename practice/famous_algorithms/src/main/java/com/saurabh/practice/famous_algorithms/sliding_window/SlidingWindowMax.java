96323275861fe509878d0db2543c5b3d866d9224
Saurabh Agarwal <Saurabh.Agarwal@softwareag.com>
2020-07-14 19:13:39 +0530

    Added sliding window max and sum using segment tree

--- practice/famous_algorithms/src/main/java/com/saurabh/practice/famous_algorithms/sliding_window/SlidingWindowMax.java
+++ practice/famous_algorithms/src/main/java/com/saurabh/practice/famous_algorithms/sliding_window/SlidingWindowMax.java
@@ -4,49 +4,49 @@ import com.saurabh.source.data_structures.SegmentTree;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /*
- Calculate the sums of sliding windows of size k in an array.
+ Calculate the maximums of sliding windows of size k in an array.
  */
-public class SlidingWindowSum {
+public class SlidingWindowMax {
   public static void main(String[] args) {
-    SlidingWindowSum obj = new SlidingWindowSum();
+    SlidingWindowMax obj = new SlidingWindowMax();
     int[] array = {6, 5, 4, 3, 2, 13, 5, 7, 8, 6, 5};
     System.out.println(Arrays.toString(obj.calculate(array, 3)));
     System.out.println(Arrays.toString(obj.calculateUsingSegmentTree(array, 3)));
   }
 
-  // Solution calculate the initial window sum, removes the first added element and adds a new element as the window slides.
-  // Complexity: O(n) time, O(1) space.
+  // Solution: Progressively calculates the max in the window as the window slides.
+  // Complexity: O(nk) time, O(1) space.
   public int[] calculate(int[] array, int k) {
     if (array == null || array.length < k) {
       throw new IllegalArgumentException();
     }
 
-    int windowSum = 0;
-    for (int i = 0; i < k; i++) {
-      windowSum += array[i];
-    }
-
+    int windowMax = 0;
     ArrayList<Integer> toReturn = new ArrayList<>();
-    toReturn.add(windowSum);
-    for (int i = 0, j = k; j < array.length; i++, j++) {
-      int beforeWindow = array[i];
-      int afterWindow = array[j];
-      windowSum = windowSum - beforeWindow + afterWindow;
-      toReturn.add(windowSum);
+    for (int i = 0, j = k; i <= array.length - k; i++, j++) {
+      windowMax = max(array, i, j);
+      toReturn.add(windowMax);
     }
 
     return toReturn.stream().mapToInt(value -> value).toArray();
   }
 
+  // Uses a segment tree to calculate window max
+  // Complexity: O(n) space, O(nlogn) time
   public int[] calculateUsingSegmentTree(int[] array, int k) {
-    SegmentTree segmentTree = new SegmentTree(array, SegmentTree.Operation.of(Integer::sum, 0));
+    SegmentTree segmentTree = new SegmentTree(array, SegmentTree.Operation.of(Integer::max, Integer.MIN_VALUE));
     ArrayList<Integer> toReturn = new ArrayList<>();
     for (int i = 0; i <= array.length - k; i++) {
       toReturn.add(segmentTree.rangeQuery(i, i + k - 1));
     }
 
     return toReturn.stream().mapToInt(value -> value).toArray();
+  }
+
+  private int max(int[] array, int i, int j) {
+    return IntStream.of(Arrays.copyOfRange(array, i, j)).max().getAsInt();
   }
 }
